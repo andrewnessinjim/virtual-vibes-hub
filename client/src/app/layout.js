1ccf37a5c018581e941ebe3a9291edc618ca9d22
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-02-28 12:14:45 +0530

    Init styled components

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import ServerUserProvider from "@/components/UserProvider";
+import GlobalStyles from "./global.styled";
+import StyledComponentsRegistry from "@/components/StyledComponentsRegistry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,10 +12,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ServerUserProvider>{children}</ServerUserProvider>
-      </body>
-    </html>
+    <StyledComponentsRegistry>
+      <html lang="en">
+        <body className={inter.className}>
+          <ServerUserProvider>{children}</ServerUserProvider>
+          <GlobalStyles />
+        </body>
+      </html>
+    </StyledComponentsRegistry>
   );
 }
