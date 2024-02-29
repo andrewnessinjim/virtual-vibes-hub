434206df63e67c096155c75fb79b27588a67f44e
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-02-29 13:28:38 +0530

    Increase background lightness

--- client/src/app/app.styled.js
+++ client/src/app/app.styled.js
@@ -3,6 +3,6 @@
 import styled from "styled-components";
 
 export const StBody = styled.body`
-    background: ${(p) => p.theme.colors.sage1};
+    background: ${(p) => p.theme.colors.sage4};
     color: ${(p) => p.theme.colors.teal12};
 `;
