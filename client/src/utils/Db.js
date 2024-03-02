01197fbee9e63e9d048d7260eeb6b36a4389f138
Andrew Nessin <andrewnessinjim@yahoo.com>
2024-03-02 19:34:19 +0530

    Make db connection code reusable

---
+++ client/src/utils/Db.js
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+let db = null;
+
+function initializeDb() {
+    const mongodbURI = "mongodb://wordle-user:wordle-pass@localhost:27017/";
+    console.log("Instantiating Mongodb client 🍃");
+    const client = new MongoClient(mongodbURI);
+    db = client.db("wordle");
+}
+
+export default function getDb(){
+    if(!db) {
+        initializeDb()
+    }
+
+    return db;
+}
--- client/src/utils/userDb.js
+++ client/src/utils/userDb.js
@@ -1,10 +1,6 @@
-import { MongoClient } from "mongodb";
+import getDb from "./Db";
 
-const mongodbURI = "mongodb://wordle-user:wordle-pass@localhost:27017/";
-console.log("Instantiating Mongodb client 🍃");
-const client = new MongoClient(mongodbURI);
-const database = client.db("wordle");
-const users = database.collection("users");
+const users = getDb().collection("users");
 
 export async function findUser(uuid) {
   return await users.findOne({ uuid });
